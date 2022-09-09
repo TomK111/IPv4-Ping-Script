31c9c7f9ba5285624c8d794563d1816ba32e653e
Tomislav Kraljic <63562144+TomislavKraljic@users.noreply.github.com>
2022-09-09 08:39:29 -0400

    Update Ping_IP_Script.ps1

--- Ping_IP_Script.ps1
+++ Ping_IP_Script.ps1
@@ -1,4 +1,4 @@
 ﻿Write-Output "Please enter IP address that you would like to ping: "
 $ip_address = Read-Host
 Write-Output "Checking Connection..."
-Test-Connection -ComputerName $ip_address -t
+Test-Connection -ComputerName $ip_address
